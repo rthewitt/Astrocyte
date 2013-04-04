ab7aed1a70f04412f92f4f19698dc5aa6668f2b0
Ryan Hewitt <rthewitt.devel@gmail.com>
2013-04-04 04:03:58 -0700

    not finding handler, but mapping seems to work.  Added partial annotations

--- astrocyte/portlets/src/main/java/com/mpi/astro/core/controller/PublicController.java
+++ astrocyte/portlets/src/main/java/com/mpi/astro/core/controller/PublicController.java
@@ -3,13 +3,11 @@ package com.mpi.astro.core.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-//@Controller
-//@RequestMapping("/delegate/bridge")
+@Controller
 public class PublicController extends AbstractController {
 	
 //	@Autowired
